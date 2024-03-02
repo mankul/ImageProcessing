9a1b33700f5d53a3b4d763d80a5f10c96b1903a7
mankul gupta <mankulgupta@gmail.com>
2024-03-02 23:26:16 +0530

    Image negative is not correctly producing for a sample image

--- ImageNegatives/main.cpp
+++ ImageNegatives/main.cpp
@@ -1,7 +1,20 @@
 #include<opencv2/opencv.hpp>
-
+#include<iostream>
 int main(){
-	cv::Mat image = cv::imread("/Users/mankulgupta/mankul/sample_images/neg1.png");
+	// path is absolute. please change accordingly.
+	//cv::Mat image = cv::imread("/Users/mankulgupta/mankul/sample_images/neg1.png");
+	cv::Mat image = cv::imread("/Users/mankulgupta/mankul/sample_images/corridor.png");
+	int L = 255;
+	int rows = image.rows;
+	int cols = image.cols;
+	std::cout<<"image size is : rows: "<<rows<<" cols: "<<cols<<std::endl;
+	for (int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
+		{
+			image.at<u_char>(i,j) = L - image.at<u_char>(i,j);
+		}
+	}
 	cv::imshow("image", image);
 	cv::waitKey(0);
 	return 0;
