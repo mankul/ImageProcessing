07d89dae7d9e3f88b536fcec36de21efe21f29ef
mankul gupta <mankulgupta@gmail.com>
2024-03-11 23:31:36 +0530

    itemp commit

--- Affine_Transformations/main.cpp
+++ Affine_Transformations/main.cpp
@@ -17,14 +17,49 @@ Mat readImage(const char * filePath){
 }
 
 
-void rotate_image(Mat image, double angle){
-	Point2f centre ((image.cols- 1)/2.0, (image.rows-1)/2.0);
+void rotate_image_complete(Mat image, double angle){
+	Mat frame, rotatedImage;
+	int x, y;
+	x = image.cols - 1;
+	y = image.rows - 1;
 
+	int diagonal = (int)std::sqrt(x * x + y * y);
+
+	Mat rotationMatrix(diagonal, diagonal, image.type());
+	Point2f centre(diagonal/2.0, diagonal/2.0);
+	
+	int offset_x = ( diagonal - x ) / 2;
+	int offset_y = ( diagonal - y ) / 2;
+
+	image.copyTo(rotationMatrix.rowRange(offset_x , offset_x + x ).colRange(offset_y, offset_y + y));
+
+	Mat rotMat = getRotationMatrix2D(centre, angle, 1.0);
+
+
+	warpAffine(rotMat, rotatedImage, rotationMatrix, rotationMatrix.size());
+
+	double radians = ( angle * M_PI ) / 180.0;
+
+	double sinR = abs(std::sin(radians));
+	double cosR = abs(std::cos(radians));
+
+
+	imshow("complete rotate",rotatedImage);
+	waitKey(0);
+}
+
+void rotate_image(Mat image, double angle){
+	int x = image.cols-1;
+	int y = image.rows-1;
+ 	Point2f centre (x/3.0, y/3.0);
+	
+	
 	// Rotation matrix
 	Mat rotation_matrix = getRotationMatrix2D(centre, angle, 1.0);
 
 	// save rotated image to following matrix.
 	Mat rotated_image;
+
 
 	// Rotate matrix with warpAffine function.
 	warpAffine(image, rotated_image, rotation_matrix, image.size());
@@ -86,10 +121,14 @@ int main(){
 	// path is absolute. please change accordingly.
 	//cv::Mat image = cv::imread("/Users/mankulgupta/mankul/sample_images/neg1.png");
 	// cv::Mat image = cv::imread("/Users/mankulgupta/mankul/sample_images/corridor.png");
-	const char * url = "https://download.samplelib.com/jpeg/sample-city-park-400x300.jpg";
+	// const char * url = "https://download.samplelib.com/jpeg/sample-city-park-400x300.jpg";
 	const char * filePath = "/Users/mankulgupta/mankul/sample_images/sample1.jpeg";
-	downloadImage(url, filePath);
+
+	// not required
+	// downloadImage(url, filePath);
 	Mat image = readImage(filePath);
-	rotate_image(image, 45);
+
+	// rotate_image(image, 45);
+	rotate_image_complete(image, 45);
 	return 0;
 }
